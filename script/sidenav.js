62d1477515d93fddf8512b9243e5f27690ad64ea
Da-ntas <hugodfl2002@gmail.com>
2022-09-04 19:48:18 -0300

    Página clientes

---
+++ script/sidenav.js
@@ -0,0 +1,7 @@
+const burguer = document.getElementById("icon-burguer")
+const close = document.getElementById("icon-close")
+const sidenav = document.getElementById("sideNav")
+
+function showSideNav() {
+    sidenav.style.display = "block !important"
+}
