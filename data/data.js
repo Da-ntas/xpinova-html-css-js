5422c27b1cdaad625f07b9121621f8319c78b621
Da-ntas <hugodfl2002@gmail.com>
2022-09-25 12:42:23 -0300

    notmain

--- data/data.js
+++ data/data.js
@@ -1,20 +1,46 @@
 export const dataCliente = [
-    {
-      "codCliente": 100,
-      "nomCliente": "XP INOVA",
-      "cnpj": "01234567901234",
-      "cpf": "012345678",
-      "codTributacao": 1,
-      "codAcessos": "19238"
-    },
-    {
-      "codCliente": 1010,
-      "nomCliente": "Cliente teste",
-      "cnpj": "01234367601234",
-      "cpf": "012342668",
-      "codTributacao": 4,
-      "codAcessos": "12378"
-    }
+  {
+    "codCliente": 1010,
+    "nomCliente": "Cliente",
+    "cnpj": "89722389148",
+    "cpf": "12342668",
+    "codTributacao": "4",
+    "codAcessos": "12378",
+    "tarefaRel": [
+      {
+        "codTarefa": 1
+      }
+    ]
+  },
+  {
+    "nomCliente": "XP INOVA",
+    "cnpj": "192837901823",
+    "cpf": "18927304981",
+    "codTributacao": "2",
+    "codAcessos": "1928341",
+    "codCliente": 1011,
+    "tarefaRel": [
+      {
+        "codTarefa": 1
+      },
+      {
+        "codTarefa": 3
+      }
+    ]
+  },
+  {
+    "codCliente": 100,
+    "nomCliente": "Editado",
+    "cnpj": "1234123901234",
+    "cpf": "12344778",
+    "codTributacao": "2",
+    "codAcessos": "8112439",
+    "tarefaRel": [
+      {
+        "codTarefa": 2
+      }
+    ]
+  }
 ]
 
 export function formatStatus(status){
@@ -23,6 +49,45 @@ export function formatStatus(status){
   if(status == "FZD") return "FAZENDO"
   if(status == "ATV") return "ATIVO"
 }
+
+export const dataTarefa = [
+  {
+    "codTarefa": 1,
+    "nomTarefa": "Emissão do MEI",
+    "descTarefa": "Emitir MEI do cliente",
+    "status": "FIN",
+    "clienteRel": [
+      {
+        "codCliente": 1010
+      },
+      {
+        "codCliente": 1011
+      }
+    ]
+  },
+  {
+    "codTarefa": 2,
+    "nomTarefa": "NAOSEI",
+    "descTarefa": "NAO SEI DO cliente",
+    "status": "PEND",
+    "clienteRel": [
+      {
+        "codCliente": 100
+      }
+    ]
+  },
+  {
+    "codTarefa": 3,
+    "nomTarefa": "SOPRATER",
+    "descTarefa": "TER DO cliente",
+    "status": "FIN",
+    "clienteRel": [
+      {
+        "codCliente": 1011
+      }
+    ]
+  }
+]
 
 export const dataTributacao = [
     {
--- script/editarTarefa.js
+++ script/editarTarefa.js
@@ -1,34 +1,65 @@
-import { usePut } from './requisitions.js';
+import { usePut, useGet } from './requisitions.js';
 
-// const queryString = window.location.search;
-// const urlParams = new URLSearchParams(queryString);
-// const codCliente = urlParams.get('codTarefa')
-// const nomCliente = urlParams.get('nomTarefa')
-// const cnpjCliente = urlParams.get('clientesAlocados')
-// const cpfCliente = urlParams.get('statusTarefa')
-// const codAcessoCliente = urlParams.get('descTarefa')
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const codTarefa = urlParams.get('codTarefa')
+let nomTarefa = urlParams.get('nomTarefa')
+const clientesAlocados = urlParams.get('clientesAlocados').split(',')
+const statusTarefa = urlParams.get('statusTarefa')
+let descTarefa = urlParams.get('descTarefa')
+const selectStatus = document.querySelector("#status")
+const checkClientes = document.querySelector("#checkClientes")
 
+if(codTarefa){
+  document.querySelector("#nomTarefa").value = nomTarefa
+  document.querySelector("#descricao").value = descTarefa
+  document.querySelector(`[value=${statusTarefa}]`).selected = true
+}
 
+let newNomTarefa = document.querySelector('#nomTarefa')
+let arrayClientes = []
+let optionSelected = document.querySelector('option:checked').value
+let newDesc = document.querySelector("#descricao")
 
-// if(codCliente){
-//     let list = document.querySelector("#tributacaoCliente")
-//     document.querySelector("#nomCliente").value = nomCliente
-//     document.querySelector("#cnpjCliente").value = cnpjCliente
-//     document.querySelector("#cpfCliente").value = cpfCliente
-//     document.querySelector("#codAcessoCliente").value = codAcessoCliente
+function handleClick(event) {
+  let valueCheck = event.target.value;
+  if(event.target.checked){
+    arrayClientes.push(valueCheck)
+  }
 
-//     dataTributacao?.map((i) => {
-//         let opt = document.createElement('option');
-//         opt.value = i.codTributacao;
-//         opt.innerHTML = i.nomTributacao;
-//         opt.selected = i.nomTributacao == nomTributacao ? true : false
-//         list.appendChild(opt)
-//     })
-// }
+  if(arrayClientes.includes(valueCheck) && !event.target.checked){
+    let index = arrayClientes.indexOf(valueCheck)
+    if(index > -1){
+      arrayClientes.splice(index, 1)
+    }
+  }
+
+  console.log(arrayClientes)
+
+};
+
+window.addEventListener('load', async (event) => {
+  let response = await useGet('/clientes')
+  let stringElement = ""
+  response?.map((i) => {
+    let check = false;
+    if(clientesAlocados.includes(i.codCliente)){
+      check = true
+      arrayClientes.push(i.codCliente )
+    }
+    
+    let element = `
+      <div class="col">
+        <input class="form-check-input selectCliente" type="checkbox" value="${i.codCliente}" onClick="handleClick(this)" name="cliente" ${!check ? "" : "checked"}>
+        <label class="form-check-label" for="status">${i.nomCliente}</label>
+      </div>
+    `
+    checkClientes.insertAdjacentHTML('beforeend',element)
+  })
+  
+})
 
 const boxes = document.querySelectorAll('.selectCliente');
-let arrayClientes = []
-
 boxes.forEach(box => {
   box.addEventListener('click', function handleClick(event) {
     let valueCheck = event.target.value;
@@ -48,26 +79,36 @@ boxes.forEach(box => {
   });
 });
 
-// document.querySelector("#salvarEditCliente")?.addEventListener('click', async (event) => {
-//     event.preventDefault();
+selectStatus.addEventListener('change', (event) => {
+  optionSelected = event.target.value
+})
+newNomTarefa.addEventListener('change', (event) => {
+  nomTarefa = event.target.value
+})
+newDesc.addEventListener('change', (event) => {
+  descTarefa = event.target.value
+})
+document.querySelector("#salvarEditCliente")?.addEventListener('click', async (event) => {
+    event.preventDefault();
 
-//     let nome = document.querySelector("#nomCliente").value
-//     let cnpj = document.querySelector("#cnpjCliente").value
-//     let cpf = document.querySelector("#cpfCliente").value
-//     let codAcesso = document.querySelector("#codAcessoCliente").value
-//     let codTributacao = document.querySelector("#tributacaoCliente").value
+    //let nome -> nomTarefa
+    //let desc -> descTarefa
+    //let status -> optionSelected
+    //let clientes
+    let arrayFormatadoClientes = arrayClientes.map((i) => {
+      return { codCliente: i}
+    })
 
-//     let body = {
-//         "codCliente": codCliente,
-//         "nomCliente": nome,
-//         "cnpj": cnpj,
-//         "cpf": cpf,
-//         "codTributacao": codTributacao,
-//         "codAcessos": codAcesso
-//     }
-//     let {data: response} = await usePut('/tarefas', body)
+    let body = {
+        "codTarefa": codTarefa,
+        "nomTarefa": nomTarefa,
+        "descTarefa": descTarefa,
+        "status": optionSelected,
+        "clienteRel": arrayFormatadoClientes
+    }
+    let {data: response} = await usePut('/tarefas', body)
 
-//     if(response){
-//         location.href = `detalhesTarefa.html?codTarefa=${response.codTarefa}`
-//     }
-// })
+    if(response){
+        location.href = `detalhesTarefa.html?codTarefa=${response.codTarefa}`
+    }
+})
